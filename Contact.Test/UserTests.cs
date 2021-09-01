9b9a7da630247f8693ea47bd43b631b8c4c5225a
AshutoshTutwani <ashutosh.tutwani@highlands.in>
2021-09-01 20:22:44 +0530

    Added test cases

--- Contact.Test/UserTests.cs
+++ Contact.Test/UserTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,7 +14,13 @@ namespace Contact.Test
 {
     public class Tests
     {
+        #region Fields
+
         private UserDetail _testUser;
+
+        #endregion
+
+        #region Setup
 
         [SetUp]
         public void Setup()
@@ -23,7 +30,7 @@ namespace Contact.Test
             {
                 FirstName = "Ashutosh",
                 LastName = "Tutwani",
-                DateOfBirth = "31/10/1992",
+                DateOfBirth = "1992-10-31",
             };
 
             _testUser.Emails.Add("ashutoshtutwani@gmail.com");
@@ -32,23 +39,31 @@ namespace Contact.Test
             _testUser.PhoneNumbers.Add("8368011119");
         }
 
+        #endregion
+
+        #region Test Methods
+
+        [TestCaseSource("InsertUsersTestCases")]
+        [Order(1)]
         [Test]
-        public async Task InsertUser()
+        public async Task InsertUser(UserDetail userDetail)
         {
             var userController = new UserController(null, new Repository());
-            var result = await userController.InsertUser(_testUser);
+            var result = await userController.InsertUser(userDetail);
             Assert.AreNotEqual(0,result.Value);
         }
 
         [Test]
+        [Order(3)]
         public async Task GetAllUsers()
         {
-           var userController = new UserController(null,new Repository());
-           var result = await userController.GetAllUsers();
-           Assert.AreEqual(true, result.Value!=null);
+            var userController = new UserController(null,new Repository());
+            var result = await userController.GetAllUsers();
+            Assert.AreEqual(true, result.Value!=null);
         }
 
         [Test]
+        [Order(2)]
         public async Task UpdateUser()
         {
             var userController = new UserController(null, new Repository());
@@ -65,5 +80,63 @@ namespace Contact.Test
             }
             
         }
+
+        #endregion
+
+        #region Methods
+
+        private static IEnumerable<UserDetail> InsertUsersTestCases()
+        {
+            yield return new UserDetail()
+            {
+                FirstName = "Jack",
+                LastName = "Nicholson",
+                DateOfBirth = "1981-09-10",
+                Emails = new List<string>()
+                {
+                    "jack.nick@gmail.com",
+                    "jack2.3nick@gmail.com",
+                },
+                PhoneNumbers = new List<string>()
+                {
+                    "9810895395",
+                    "4423221322",
+                },
+            };
+
+            yield return new UserDetail()
+            {
+                FirstName = "Tom",
+                LastName = "Hanks",
+                DateOfBirth = "1991-12-19",
+                Emails = new List<string>()
+                {
+                    "tom.hanks@reddif.com",
+                    "tomactor.hanks@gmail.com",
+                },
+                PhoneNumbers = new List<string>()
+                {
+                    "1043895395",
+                    "7423521322",
+                },
+            };
+
+            yield return new UserDetail()
+            {
+                FirstName = "David",
+                LastName = "Miller",
+                DateOfBirth = "1971-09-10",
+                Emails = new List<string>()
+                {
+                    "david.miller@gmail.com",
+                },
+                PhoneNumbers = new List<string>()
+                {
+                    "7723221322",
+                },
+            };
+        }
+
+        #endregion
     }
 }
